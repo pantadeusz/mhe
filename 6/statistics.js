2e033a24acf8710a2b45883fbc5a92fea0481723
pantadeusz <tadeusz@puzniakowski.pl>
2019-10-24 15:26:22 +0200

    zbieranie danych

--- 6/statistics.js
+++ 6/statistics.js
@@ -6,29 +6,31 @@ const fs = require('fs');
 
 let summary_stats = {};
 fs.readdirSync(experiment_folder).forEach(file => {
-    var test = file.match(/(.*)-(.*)-(.*)\.json/);
-    const experiment_meta = {
-        "method": test[1],
-        "experiment_input_size": test[2],
-        "i": test[3]
-    };
-    experiment_data = JSON.parse(fs.readFileSync(`${experiment_folder}/${file}`, 'utf8'));
-    if (summary_stats[experiment_meta.method]) {
-        if (summary_stats[experiment_meta.method][experiment_meta.experiment_input_size]) {
-        summary_stats[experiment_meta.method]
-        [experiment_meta.experiment_input_size].push(experiment_data.goal);
-        }
-        else {
+    if (/(.*)-(.*)-(.*)\.json/.test(file)) {
+        var test = file.match(/(.*)-(.*)-(.*)\.json/);
+        const experiment_meta = {
+            "method": test[1],
+            "experiment_input_size": test[2],
+            "i": test[3]
+        };
+        experiment_data = JSON.parse(fs.readFileSync(`${experiment_folder}/${file}`, 'utf8'));
+        if (summary_stats[experiment_meta.method]) {
+            if (summary_stats[experiment_meta.method][experiment_meta.experiment_input_size]) {
+                summary_stats[experiment_meta.method]
+                [experiment_meta.experiment_input_size].push(experiment_data.goal);
+            }
+            else {
+                summary_stats[experiment_meta.method][experiment_meta.experiment_input_size] =
+                    [experiment_data.goal]
+            }
+        } else {
+            summary_stats[experiment_meta.method] = {}
+            summary_stats[experiment_meta.method] = {}
             summary_stats[experiment_meta.method][experiment_meta.experiment_input_size] =
                 [experiment_data.goal]
         }
-    } else {
-        summary_stats[experiment_meta.method] = {}
-        summary_stats[experiment_meta.method] = {}
-        summary_stats[experiment_meta.method][experiment_meta.experiment_input_size] =
-            [experiment_data.goal]
+        //console.log(experiment_meta);
     }
-    console.log(experiment_meta);
 });
 console.log(summary_stats)
 for (let method_name in summary_stats) {
@@ -37,8 +39,8 @@ for (let method_name in summary_stats) {
         let n = 0;
         for (v of summary_stats[method_name][experiment_input_size]) {
             s += v;
-            n ++;
+            n++;
         }
-        console.log(`${method_name} ${experiment_input_size} ${s / n} ${(s / n) / summary_stats["brute_force_find_solution"][experiment_input_size][0] -1.0}`)
+        console.log(`${method_name} ${experiment_input_size} ${s / n} ${(s / n) / summary_stats["brute_force_find_solution"][experiment_input_size][0] - 1.0}`)
     }
 }
