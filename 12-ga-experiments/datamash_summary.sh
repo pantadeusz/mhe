9489d50a46b78535aaaba52a1795cabeb1de88bf
pantadeusz <tadeusz@puzniakowski.pl>
2020-01-09 13:40:10 +0100

    datamash incorporation

--- 12-ga-experiments/datamash_summary.sh
+++ 12-ga-experiments/datamash_summary.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+export LANG=C
+
 for i in __summary_*.txt; do
 echo "mashing $i"
-datamash -t' ' --sort --group 2,3,4,5,6,7 mean 8,9 < $i > $i.averages.csv
+datamash -t' ' --sort --group 3,4,5,6,7,8 mean 9,10 < $i > $i.averages_times_results.csv
 done
