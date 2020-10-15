fd873f460987512475b47bf044eb375fa765c1bc
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2020-10-15 12:17:16 +0200

    prepare for new semester

--- dzienne_2019_2020/12-ga-experiments/statistics_summary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-P=$PWD
-echo "collecing results"
-for current_dir in output/*; do
-cd $current_dir
-for i in *.txt; do echo ${i%.*}=$(cat $i | grep calculation_time | awk '{print $3}')=$(cat $i | grep goal_value | awk '{print $3}') | tr '=' ' ' ; done > $P/__summary_$(basename $current_dir).txt
-cd $P
-done
