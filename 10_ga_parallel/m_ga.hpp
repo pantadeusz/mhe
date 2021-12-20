2a850f5a8d24ff2688559d72c6a2f8e5b4eb0692
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2021-12-20 23:29:24 +0100

    crossover and mutation for genetic algorithm and TSP problem 2

---
+++ 10_ga_parallel/m_ga.hpp
@@ -0,0 +1,69 @@
+#ifndef __M_GA_____HPP___
+#define __M_GA_____HPP___
+
+
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <random>
+#include <set>
+
+
+std::random_device rd;
+std::mt19937 randgen(rd());
+
+
+auto genetic_algorithm = [](
+                             auto fitness,
+                             auto population,
+                             auto term_condition, // int iterations,
+                             auto select,
+                             auto crossover,
+                             double p_crossover,
+                             auto mutation,
+                             double p_mutation,
+                             std::function<void(int c, double dt)> on_statistics = [](int c, double dt) {},
+                             std::function<void(int i, double current_goal_val, double goal_val)> on_iteration = [](int i, double current_goal_val, double goal_val) {}) {
+    using namespace std;
+    auto start = chrono::steady_clock::now();
+    int iteration = 0;
+
+    vector<double> population_fit(population.size());
+    std::transform(population.begin(), population.end(), population_fit.begin(), fitness);
+    while (term_condition(population, population_fit, iteration++)) {
+        decltype(population) new_pop(population.size());
+        for (int c = 0; c < population.size(); c += 2) {
+            uniform_real_distribution<double> should_mutate;
+            uniform_real_distribution<double> should_crossover;
+            auto parent1 = population.at(select(population_fit));
+            auto parent2 = population.at(select(population_fit));
+            auto [child1, child2] = (should_crossover(randgen) < p_crossover) ? crossover(parent1, parent2) : make_pair(parent1, parent2);
+            if (should_mutate(randgen) < p_mutation) {
+                child1 = mutation(child1);
+            }
+            if (should_mutate(randgen) < p_mutation) {
+                child2 = mutation(child2);
+            }
+            new_pop[c + 0] = child1;
+            new_pop[c + 1] = child2;
+        }
+        population = new_pop;
+        std::transform(population.begin(), population.end(), population_fit.begin(), fitness);
+
+        auto best_specimen_it = max_element(population_fit.begin(), population_fit.end(), [&](auto a, auto b) {
+            return a < b;
+        });
+        on_iteration(iteration, *best_specimen_it, *best_specimen_it);
+    }
+
+
+    auto finish = chrono::steady_clock::now();
+    chrono::duration<double> duration = finish - start;
+    on_statistics(iteration, duration.count());
+    return population;
+};
+
+#endif
---
+++ 10_ga_parallel/tsp_problem.hpp
@@ -0,0 +1,88 @@
+#ifndef __TSP_PROBLEM_IMPLEMENTATION___
+#define __TSP_PROBLEM_IMPLEMENTATION___
+
+#include <functional>
+#include <tuple>
+#include <vector>
+
+/**
+ * graph representing problem
+ * */
+using my_graph_t = std::vector<std::vector<double>>;
+/*
+list of the cities - indexes
+*/
+using work_point_t = std::vector<int>;
+
+/**
+ * generates cost function for TSP
+ * */
+std::function<double(work_point_t)> cost_function_factory(my_graph_t tsp_problem);
+
+/**
+ * this generates random problem (set of cities) for TSP
+ *
+ * it will provide information about the cities coordinates via ```on_cities``` callback
+ * */
+my_graph_t generate_random_problem(
+    int n,
+    std::function<void(std::vector<std::pair<double, double>>)> on_cities = [](auto cities_list) {});
+
+/**
+ * thid function loads the graph from file
+ * */
+my_graph_t load_problem(std::string fname);
+
+/**
+ * this function loads list of cities and then generates graph
+ * */
+my_graph_t load_problem_from_coordinates(std::vector<std::pair<double, double>> cities_list);
+/**
+ * this function generates random path in graph.
+ * */
+work_point_t generate_random_tsp_point(my_graph_t problem);
+
+/**
+ * it will generate first solutino for TSP problem - it is the list of cities to see [0, 1, 2, ... ,n-1]
+ * */
+work_point_t generate_first_tsp_point(my_graph_t problem);
+
+/**
+ * generates some random neighbour of the curent work point. The returned value is
+ * "close" to the p
+ * **/
+work_point_t generate_random_tsp_neighbour(work_point_t p);
+
+
+/**
+ * generates some random neighbour of the curent work point. The returned value is
+ * "close" to the p
+ * **/
+std::vector<work_point_t> generate_tsp_point_neighbours(const work_point_t p);
+
+
+/**
+ * this function will give the next point for TSP
+ * */
+work_point_t get_next_point(work_point_t p);
+
+/**
+ * this function allows to compare two solutions for TSP
+ * */
+bool operator==(work_point_t a, work_point_t b);
+
+
+/**
+ * output operator that allows to writing problem data to stream
+ * */
+std::ostream& operator<<(std::ostream& o, const my_graph_t graph);
+
+/**
+ * this will output the solution to the stream.
+ * */
+std::ostream& operator<<(std::ostream& o, const std::pair<my_graph_t, work_point_t> graph_w_solution);
+
+// vector<pair<double, double>>
+std::ostream& operator<<(std::ostream& o, const std::pair<std::vector<std::pair<double, double>>, work_point_t> graph_w_solution);
+
+#endif
