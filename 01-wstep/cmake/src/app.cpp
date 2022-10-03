cc81926df05f09aeb9ffd55c18488e8a0d3ca2a8
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2022-10-03 14:36:53 +0200

    przyklad

--- 01-wstep/cmake/src/app.cpp
+++ 01-wstep/cmake/src/app.cpp
@@ -1,5 +1,10 @@
+#include <fstream>
+#include <functional>
 #include <iostream>
+#include <json.hpp>
+#include <memory>
 #include <string>
+#include <tuple>
 #include <vector>
 
 class MojaKlasa {
@@ -38,14 +43,42 @@ void MojaKlasa::ktoja() { std::cout << "Jam ci " << id << std::endl; }
 
 void wypisz() { std::cout << "Jestem sobie funkcja" << std::endl; }
 
+class InnyTyp {
+ public:
+  int x;
+  std::string b;
+};
+
+std::function<void()> fabryka() {
+  auto i = std::make_shared<int>(10);
+  return [=]() {
+    std::cout << "i = " << *i << std::endl;
+    (*i)++;
+  };
+}
+
 int main(int argc, char **argv) {
   using namespace std;
   MojaKlasa x = {99};
   x.ktoja();
+  auto f = fabryka();
+  f();
+  vector<InnyTyp> tablica = {{1, "jeden"}, {2, "dwa"}};
+  for (auto &[i, c] : tablica) {
+    cout << "element: " << i << " " << c << endl;
+    i = -i;
+  }
+  for (auto &[i, c] : tablica) {
+    cout << "po zmianie: " << i << " " << c << endl;
+  }
 
-  auto y = wypisz;
-
-  y();
-
+  f();
+  f();
+  f();
+  std::ifstream file("test.json");
+  nlohmann::json data = nlohmann::json::parse(file);
+  std::cout << data.dump() << std::endl;
+  nlohmann::json j = {{"sdasf",22},{"r","1122"}};
+  std::cout << data["liczba"] << std::endl;
   return 0;
 }
